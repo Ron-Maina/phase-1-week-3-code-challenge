36f5595d6094b702f299eb7dc57800b09427470a
Ron <ronmwangi00@gmail.com>
2023-07-01 23:25:07 +0300

    Displayed all movie selections to the left

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-//Render first movie
-fetch('http://localhost:3000/films')
-.then(res => res.json())
-.then(filmData => {
-    let movie = document.createElement('div')
-    movie.className='movie'
-    movie.innerHTML =`
-    <img src="${filmData[0].poster}">
-    <h4>${filmData[0].title}</h4>
-    <p>Showtime: ${filmData[0].showtime}</p>
-    <p>Runtime: ${filmData[0].runtime}</p>
-    <P>Available tickets: ${filmData[0].tickets_sold}</p>
-    `
-    console.log(movie)
-    let main = document.querySelector('main')
-    main.querySelector('#selected-movie').appendChild(movie)
+document.addEventListener('DOMContentLoaded', () => {
+    //Render first movie
+    fetch('http://localhost:3000/films')
+    .then(res => res.json())
+    .then(filmData => {
+        let movie = document.createElement('div')
+        movie.className='movie'
+        movie.innerHTML =`
+        <img src="${filmData[0].poster}">
+        <h4>${filmData[0].title}</h4>
+        <p>Showtime: ${filmData[0].showtime}</p>
+        <p>Runtime: ${filmData[0].runtime}</p>
+        <P>Available tickets: ${filmData[0].tickets_sold}</p>
+        `
+        console.log(movie)
+        let main = document.querySelector('main')
+        main.querySelector('#selected-movie').appendChild(movie)
+
+    })
+
+    //display the rest of the movies
+    fetch('http://localhost:3000/films')
+    .then(res => res.json())
+    .then(films => films.forEach(film => renderAllFilms(film)))
+
+    //function to handle rendering the rest of the films
+    function renderAllFilms(film){
+        let movieList = document.createElement('li')
+        movieList.className = 'all-movies'
+        movieList.innerHTML = `
+        <img src = "${film.poster}">
+        <h4>${film.title}</h4>
+        `
+        let main = document.querySelector('main')
+        main.querySelector('#films').appendChild(movieList)
+    }
+
+
+
 
 })
 
-// function renderFirstMovie(filmData){
-    
-//     let movie = document.createElement('div')
-//     movie.className='movie'
-//     movie.innerHtml = `
-//     <img src="${filmData[0].poster}"
-//     <h4>${filmData[0].title}</h4>
-//     <p>Showtime: ${filmData[0].showtime}</p>
-//     <p>Runtime: ${filmData[0].runtime}</p>
-//     <P>Available tickets: ${filmData[0].tickets_sold}</p>
-//     `
-//     console.log(movie)
-//     let main = document.querySelector('main')
-//     main.querySelector('#selected-movie').appendChild(movie)
-// }
+
 
