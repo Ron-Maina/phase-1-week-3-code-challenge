e8e2037f031c05c8f07d5729263aebe04c112032
Ron <ronmwangi00@gmail.com>
2023-07-02 04:09:01 +0300

    Displaying selected movie details to the DOM

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
         <h4>${filmData[0].title}</h4>
         <p>Showtime: ${filmData[0].showtime}</p>
         <p>Runtime: ${filmData[0].runtime}</p>
-        <P>Available tickets: ${filmData[0].tickets_sold}</p>
+        <P>Available tickets: ${filmData[0].capacity - filmData[0].tickets_sold}</p>
         <button class="btn">Buy Ticket</button>
         `
         console.log(movie)
         let main = document.querySelector('main')
         main.querySelector('#selected-movie').appendChild(movie)
-
     })
 
     //display the rest of the movies
@@ -34,7 +33,31 @@ document.addEventListener('DOMContentLoaded', () => {
         `
         let main = document.querySelector('main')
         main.querySelector('#films').appendChild(movieList)
+
+        //add event listener to movie titles
+        console.log(movieList.querySelector('h4'))
+        movieList.querySelector('h4').addEventListener('click', () => {
+            // function displaySelectedMovie(){
+            document.querySelector('#selected-movie').innerHTML=''
+            let selectedMovie = document.createElement('div')
+            selectedMovie.className='movie'
+            selectedMovie.innerHTML=`
+            <img src = "${film.poster}">
+            <h4>${film.title}</h4>
+            <p>Showtime: ${film.showtime}</p>
+            <p>Runtime: ${film.runtime}</p>
+            <p>Available Tickets: ${film.capacity - film.tickets_sold}</p>
+            <button class="btn">Buy Ticket</button>
+            `
+            let main = document.querySelector('main')
+            main.querySelector('#selected-movie').appendChild(selectedMovie)
+        })
+
+      
+        
     }
+
+
 
 
 
