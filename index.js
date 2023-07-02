13fb6169723b9455a9478a7256d5541cd6819359
Ron <ronmwangi00@gmail.com>
2023-07-02 13:50:01 +0300

    Added buy ticket functionality

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
         <h4>${filmData[0].title}</h4>
         <p>Showtime: ${filmData[0].showtime}</p>
         <p>Runtime: ${filmData[0].runtime}</p>
-        <P>Available tickets: ${filmData[0].capacity - filmData[0].tickets_sold}</p>
+        <P class="tickets">Available tickets: ${filmData[0].capacity - filmData[0].tickets_sold}</p>
         <button class="btn">Buy Ticket</button>
         `
         console.log(movie)
         let main = document.querySelector('main')
         main.querySelector('#selected-movie').appendChild(movie)
+
+        let availableTickets = (filmData[0].capacity - filmData[0].tickets_sold)
+        document.querySelector('.btn').addEventListener('click', () => {
+            availableTickets--
+            document.querySelector('.tickets').textContent = "Available Tickets:" + availableTickets
+            
+        })   
     })
 
     //display the rest of the movies
@@ -37,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
         //add event listener to movie titles
         console.log(movieList.querySelector('h4'))
         movieList.querySelector('h4').addEventListener('click', () => {
-            // function displaySelectedMovie(){
             document.querySelector('#selected-movie').innerHTML=''
             let selectedMovie = document.createElement('div')
             selectedMovie.className='movie'
@@ -46,11 +52,21 @@ document.addEventListener('DOMContentLoaded', () => {
             <h4>${film.title}</h4>
             <p>Showtime: ${film.showtime}</p>
             <p>Runtime: ${film.runtime}</p>
-            <p>Available Tickets: ${film.capacity - film.tickets_sold}</p>
+            <p class="tickets">Available Tickets: ${film.capacity - film.tickets_sold}</p>
             <button class="btn">Buy Ticket</button>
             `
             let main = document.querySelector('main')
             main.querySelector('#selected-movie').appendChild(selectedMovie)
+
+            //Buy ticket event listener
+            let availableTickets = (film.capacity - film.tickets_sold)
+            document.querySelector('.btn').addEventListener('click', () => {
+                availableTickets--
+                document.querySelector('.tickets').textContent = "Available Tickets:" + availableTickets
+                
+            })   
+               
+           
         })
 
       
